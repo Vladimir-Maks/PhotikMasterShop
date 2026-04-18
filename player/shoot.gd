d03f730499de9bc737c69efbed5ce3568e0409e2
SawTrem <57016613+SawTrem@users.noreply.github.com>
2026-04-18 22:52:57 +0300

    CameraItem in shoot script rename

--- player/shoot.gd
+++ player/shoot.gd
@@ -3,4 +3,4 @@ extends Node
 
 func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("ui_accept"):
-		$"../../../Hands/Node3D".camera_shot.shot()
+		$"../../../Hands/CameraItem".camera_shot.shot()
