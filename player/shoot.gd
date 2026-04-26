a6e1491563002de1b0f3ecbcf25fdd40ad517dfc
Vladimir Durmanov <vladimir.max3000@gmail.com>
2026-04-26 23:49:01 +0500

    added rendering of photo on shot with simple photo-show animation

--- player/shoot.gd
+++ player/shoot.gd
@@ -1,6 +1,17 @@
 extends Node
 
+@onready var camera_item : CameraItem = %CameraItem
+@onready var fps_subviewport : SubViewport = %FPVSubViewport
+@onready var photo_screen : PhotoScreen = %PhotoScreen
+@onready var fps_camera : Camera3D = %FpsCamera3D
 
-func _physics_process(delta: float) -> void:
+func _physics_process(_delta: float) -> void:
 	if Input.is_action_just_pressed("ui_accept"):
-		$"../../../Hands/CameraItem".camera_shot.shot()
+		var result := camera_item.camera_shot.shot()
+		
+		if result:
+			fps_camera.global_transform = camera_item.global_transform
+			fps_subviewport.render_target_update_mode = SubViewport.UPDATE_ONCE
+			var photo : Texture2D = fps_subviewport.get_texture()
+			photo_screen.show_photo(photo)
+			print(photo.get_size())
---
+++ player/ui/photo_screen.gd
@@ -0,0 +1,22 @@
+extends Node
+
+class_name PhotoScreen
+
+@onready var photo_rect : TextureRect = %PhotoRect
+@onready var fade_out : AnimationPlayer = %AnimationPlayer
+@onready var timer : Timer = %Timer
+
+func _ready() -> void:
+	photo_rect.self_modulate.a = 0
+
+func show_photo(new_photo : Texture2D, want_fadeout : bool = true) -> void:
+	if new_photo:
+		photo_rect.set_texture(new_photo)
+	photo_rect.self_modulate.a = 1.0
+	
+	if want_fadeout:
+		timer.start()
+	
+
+func _on_timer_timeout() -> void:
+	fade_out.play("fade_out")
