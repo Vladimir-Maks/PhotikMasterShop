5702495d3aab9beb878265d3b40f4e9a9a0bf418
SawTrem <57016613+SawTrem@users.noreply.github.com>
2026-05-04 17:49:21 +0300

    Camera Rotation input remove, add Targeting base
    Removed rotation.
    Added Better targeting base (dictionary of multiple targets). TODO: weights (priority?), following mouse cursor (for better visible distance in front of the character)

--- player/camera.gd
+++ player/camera.gd
@@ -3,18 +3,23 @@ extends Node
 @onready var cam : Camera3D = %Camera3D
 @onready var pivot: Node3D = %CameraPivot
 @onready var anchor: SpringArm3D = %CameraAnchor
+@onready var player_cam_target: Node3D = %PlayerCameraTarget
 
 @export_group("Following Target")
-@export var target: Node3D
+@export var targets: Dictionary[String, Node3D] # Temp, better switch to weight/priority system
 @export var follow_speed: float = 5.0
 @export var offset: Vector3 = Vector3(0, 1.5, 0)
+var next_following_pos: Vector3 = Vector3.ZERO
 
-@export_group("Rotation")
+# --- DEPRECATED, but can be used for cutscenes
+@export_group("Rotation") 
 @export_range(0.0, 1.0) var mouse_sensitivity = 0.3
 @export var rotation_speed: float = 15.0
 @export var pitch_limit: Vector2 = Vector2(-50, -15)
 var yaw: float = 45.0
 var pitch: float = -60.0
+var is_rotating: bool = false
+# ---
 
 @export_group("Distance")
 @export var default_distance: float = 14.0
@@ -34,34 +39,35 @@ var distance_percentage: float
 @export var max_fov: float = 75.0
 var fov: float = 75.0
 
-var is_rotating: bool = false
-var last_mouse_pos: Vector2 = Vector2.ZERO
-
 func _ready() -> void:
-	Input.mouse_mode = Input.MOUSE_MODE_CONFINED
-	if not target:
-		target = %PlayerCameraTarget
+	if not targets:
+		add_target(player_cam_target)
 	cam.global_transform = pivot.global_transform
+	add_target(get_tree().root.get_child(0).get_node("TestCamBox")) # test target system
 	
 	if anchor:
 		anchor.spring_length = default_distance
 		to_distance = default_distance 
 		#spring_arm.add_excluded_object(target.get_rid()) # ooooh collisions oooooh
 
+func add_target(new_target: Node3D):
+	if new_target:
+		targets[new_target.name] = new_target
+
+func remove_target(old_target: Node3D):
+	targets.erase(old_target.name)
+
+func flush_targets(reset_to_player: bool = true):
+	targets.clear()
+	if reset_to_player:
+		add_target(player_cam_target)
+
 func _physics_process(delta: float) -> void:
 	if anchor:
 		_process_distance(delta)
-		_follow_target(delta)
-		_apply_rotation(delta)
+		_follow_targets(delta)
+		_apply_rotation(delta) # DEPRECATED, but can be used for cutscenes
 	cam.global_transform = pivot.global_transform
-
-func _process(delta: float) -> void:
-	Input.MOUSE_MODE_CONFINED
-	if is_rotating:
-		Input.mouse_mode = Input.MOUSE_MODE_CONFINED_HIDDEN
-		return
-	last_mouse_pos = $"../../Viewport".get_viewport().get_mouse_position() # test
-	
 
 func _process_distance(delta: float) -> void:
 	anchor.spring_length = lerp(anchor.spring_length, to_distance, zoom_speed * delta)  # YOU SHOULD LERP THIS SHIT NOW
@@ -70,21 +76,34 @@ func _process_distance(delta: float) -> void:
 	fov = clamp(min_fov + (max_fov * 2 - min_fov) * distance_percentage, min_fov, max_fov)
 	anchor.rotation.x = lerp_angle(anchor.rotation.x, deg_to_rad(-pitch), zoom_speed * delta)
 	
+func _follow_targets(delta: float) -> void:
+	next_following_pos =  anchor.global_position.lerp(_targets_midpoint(), follow_speed * delta) # smooooooooooth
+	anchor.global_position.x = next_following_pos.x
+	anchor.global_position.z = next_following_pos.z
 
-func _follow_target(delta: float) -> void:
-	anchor.global_position = anchor.global_position.lerp(target.global_position, follow_speed * delta) # smooooooooooth
+func _targets_midpoint() -> Vector3: # Or Vector3
+	if targets.is_empty():
+		printerr("No camera targets?")
+		return Vector3.ZERO
 
+	var center = Vector3.ZERO
+	for target in targets.values():
+		center += target.global_position
+
+	return center / targets.size()
+
+# --- DEPRECATED, but can be used for cutscenes
 func _apply_rotation(delta: float) -> void:
 	anchor.rotation.y = lerp_angle(anchor.rotation.y , deg_to_rad(yaw), rotation_speed * delta)
+# ---
 
 func _unhandled_input(event: InputEvent) -> void:
-	if event is InputEventMouseMotion and Input.is_action_pressed("camera_free_look"):
-		yaw -= event.screen_relative.x * mouse_sensitivity
-		is_rotating = true
-	else: is_rotating = false
-	
-	if Input.is_action_just_released("camera_free_look"):
-		Input.warp_mouse(last_mouse_pos)
+	# --- DEPRECATED, but can be used for cutscenes
+	#if event is InputEventMouseMotion and Input.is_action_pressed("camera_free_look"):
+		#yaw -= event.screen_relative.x * mouse_sensitivity
+		#is_rotating = true
+	#else: is_rotating = false
+	# ---
 
 	if Input.is_action_pressed("camera_zoom_in"):
 		to_distance = clamp(to_distance - zoom_step, min_distance, max_distance)
--- player/cursor.gd
+++ player/cursor.gd
@@ -4,11 +4,10 @@ extends Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	Input.mouse_mode = Input.MOUSE_MODE_CONFINED_HIDDEN
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if $"../Camera".is_rotating:
-		return
 	cursor.position = get_viewport().get_mouse_position()
