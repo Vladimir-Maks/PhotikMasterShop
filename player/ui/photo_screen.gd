89bee53f5dd83d387fbe0bb1dd09af35f4b7a6dc
Vladimir Durmanov <v.durmanov@ts-g.local@fedora.ts-g.local>
2026-05-04 17:10:47 +0500

    refactored camera code and merged into one script

--- player/ui/photo_screen.gd
+++ player/ui/photo_screen.gd
@@ -6,16 +6,33 @@ class_name PhotoScreen
 @onready var fade_out : AnimationPlayer = %AnimationPlayer
 @onready var timer : Timer = %Timer
 
-func _ready() -> void:
-	modulate.a = 0
+@onready var background : Panel = %PanelBackground
 
-func show_photo(new_photo : Texture2D, want_fadeout : bool = true) -> void:
-	if new_photo:
-		photo_rect.set_texture(new_photo)
+func _ready() -> void:
+	modulate.a = 0.0
+	background.modulate.a = 0.0
+
+func set_target_texture(target_texture : Texture2D) -> void:
+	photo_rect.set_texture(target_texture)
+
+func show_preview() -> void:
 	modulate.a = 1.0
+	background.modulate.a = 0.0
+
+func hide_preview() -> void:
+	modulate.a = 0.0
+	background.modulate.a = 1.0
+
+func show_photo(want_fadeout : bool = true) -> void:
+	modulate.a = 1.0
+	background.modulate.a = 1.0
 	
 	if want_fadeout:
 		timer.start()
+
+func hide_photo() -> void:
+	modulate.a = 0.0
+	background.modulate.a = 0.0
 
 func _on_timer_timeout() -> void:
 	fade_out.play("fade_out")
